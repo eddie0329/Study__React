397e5d1022096967fb4cad4a40ec19f88f128715
eddie0329 <fdsa0329@naver.com>
2019-06-23 19:09:17 +0900

    29'30'' done

---
+++ awesome-project/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Todos from './components/Todos'
+
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with Boss',
+        completed: false
+      },
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>);
+  }
+}
+
+
+export default App;
