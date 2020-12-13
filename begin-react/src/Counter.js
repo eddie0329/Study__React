58bffbd9f952923a5586c388852e4098d340cdcd
Eddie <40482274+eddie0329@users.noreply.github.com>
2020-12-13 21:39:33 +0900

    :sparkles: Implement useReducer

--- begin-react/src/Counter.js
+++ begin-react/src/Counter.js
@@ -1,15 +1,43 @@
-import React, { useState } from "react";
+import React, { useRef, useReducer, useCallback } from "react";
+
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const SET_COUNT = "SET_COUNT";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    case SET_COUNT:
+      state = action.value;
+      return state;
+    default:
+      return state;
+  }
+}
 
 function Counter() {
-  const [number, setNumber] = useState(0);
+  const countInput = useRef();
+  const [number, dispatch] = useReducer(reducer, 0);
+
   const onIncrease = () => {
-    setNumber((prevNumber) => prevNumber + 1);
+    dispatch({ type: INCREMENT });
   };
   const onDecrease = () => {
-    setNumber(number - 1);
+    dispatch({ type: DECREMENT });
   };
+  const onClickEnter = useCallback(() => {
+    const countValue = parseInt(countInput.current.value, 10);
+    dispatch({ type: SET_COUNT, value: countValue });
+    countInput.current.value = "";
+  }, []);
+
   return (
     <div>
+      <input ref={countInput} />
+      <button onClick={onClickEnter}>ENTER</button>
       <h1>{number}</h1>
       <button onClick={onIncrease}>+</button>
       <button onClick={onDecrease}>-</button>
